c3ffec106ea61a0ad68db02323d49a6761e36033
Park donghyeon <emma415g@gmail.com>
2022-10-19 01:07:25 +0900

    Read account info, ATA(Associated Token Account)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,14 @@ use solana_program::{
     native_token::LAMPORTS_PER_SOL,
     pubkey::Pubkey,
 };
-use spl_token::state::Account as TokenAccount;
+use solana_program::program_pack::Pack;
+use solana_sdk::commitment_config::CommitmentConfig;
+use spl_token::state::{
+    Account as TokenAccount,
+    Mint,
+};
+
+const USDC_ADDR: &str = "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU";
 
 fn main() {
     let client = RpcClient::new("https://api.devnet.solana.com");
@@ -16,14 +23,20 @@ fn main() {
 
     let accounts = client.get_multiple_accounts(&keys).unwrap();
     for (key, account) in std::iter::zip(keys, accounts).into_iter() {
-        println!("{}: {}.{:09}",
+        println!("{}: {}.{:09} SOL",
             key,
             account.as_ref().unwrap().lamports/LAMPORTS_PER_SOL,
             account.as_ref().unwrap().lamports%LAMPORTS_PER_SOL,
         );
     }
 
-    // client.get_token_account()
-    // client.get_account_with_commitment("")
+    let usdc_pubkey = Pubkey::from_str(USDC_ADDR).unwrap();
+    let usdc_mint_data = client.get_account_data(&usdc_pubkey).unwrap();
+    let usdc_mint = Mint::unpack(usdc_mint_data.as_slice()).unwrap();
 
+    let commitment = CommitmentConfig::finalized();
+    let me_usdc_pubkey = Pubkey::from_str("DmNbvoX6X2T5f4hfc11JyLJUQpnQ6V6XBFQXerJtJ38D").unwrap();
+    let usdc_token_account = client.get_token_account(&me_usdc_pubkey).unwrap().unwrap();
+    println!("{:?}", usdc_mint);
+    println!("{:?}", usdc_token_account);
 }
